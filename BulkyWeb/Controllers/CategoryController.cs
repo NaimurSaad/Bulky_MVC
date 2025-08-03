621a49cb006bd90134380ccdc796c93531786caa
NaimurSaad <naimursaad.ns@gmail.com>
2025-08-04 00:44:17 +0600

    updated

---
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        //#1
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList(); //#1
+            return View(objCategoryList);
+        }
+    }
+    //retrive data #1
+}
